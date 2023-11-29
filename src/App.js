31c5ff054937466f31bfc421453ee8b911f10097
AdamHong <juerlain@gmail.com>
2023-11-29 22:18:38 +0700

    2023-11-29实现了电影页面的效果

--- src/App.js
+++ src/App.js
@@ -1,11 +1,67 @@
-import React from "react";
+import { useEffect } from "react";
+
+import "./App.css";
+import searchIcon from "./search.svg";
+
+const API_URL = "http://www.omdbapi.com/?apikey=b40402c1&";
+
+const movie1 = {
+  Title: "Batman v Superman: Dawn of Justice",
+  Year: "2016",
+  imdbID: "tt2975590",
+  Type: "movie",
+  Poster:
+    "https://m.media-amazon.com/images/M/MV5BYThjYzcyYzItNTVjNy00NDk0LTgwMWQtYjMwNmNlNWJhMzMyXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+};
 
 const App = () => {
+  const searchMovie = async (title) => {
+    const response = await fetch(`${API_URL}&S=${title}`);
+    const data = await response.json();
+
+    console.log(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovie("Superman");
+  }, []);
+
   return (
-    <div className="App">
-      <h1>hello</h1>
+    <div className="app">
+      <h1>MovieLand</h1>
+
+      <div className="search">
+        <input
+          placeholder="Search for movies"
+          value="Superman"
+          onChange={() => {}}
+        />
+        <img src={searchIcon} alt="search" onClick={() => {}} />
+      </div>
+
+      <div className="container">
+        <div className="movie">
+          <div>
+            <p>{movie1.Year}</p>
+          </div>
+          <div>
+            <img
+              src={
+                movie1.Poster !== "N/A"
+                  ? movie1.Poster
+                  : "https://via.placeholder.com/400"
+              }
+            ></img>
+          </div>
+          <div>
+            <span>{movie1.Type}</span>
+            <h3>{movie1.Title}</h3>
+          </div>
+        </div>
+      </div>
+      
     </div>
   );
-}
+};
 
 export default App;
